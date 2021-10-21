d9bd3d4ee7dfdb3bf078e5673297d127dc931065
morgan4080 <morganmurungi@live.com>
2021-10-21 15:07:08 +0300

    upthegun

---
+++ layouts/default.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="relative font-body bg-gray-100 min-h-screen w-full">
+    <Nuxt />
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style>
+html {
+  scroll-behavior: smooth;
+}
+
+/* width */
+::-webkit-scrollbar {
+  width: 14px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #ffffff;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #52596d;
+  border: 2px #fcf9ed solid;
+  border-radius: 25px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: linear-gradient(to bottom, rgba(31,136,203,0.63), #ed64a6, #ea7500);
+}
+
+.scrollbar-w-2::-webkit-scrollbar {
+  width: .5rem!important;
+  height: .5rem!important;
+}
+
+.slide-bottom-enter-active,
+.slide-bottom-leave-active {
+  transition: opacity 0.25s ease-in-out, transform 0.25s ease-in-out;
+}
+.slide-bottom-enter,
+.slide-bottom-leave-to {
+  opacity: 0;
+  transform: translate(0, 6px);
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      display: ['Inter', 'system-ui', 'sans-serif'],
+      body: ['Inter', 'system-ui', 'sans-serif']
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      amber: colors.amber,
+      black: colors.black,
+      blue: colors.blue,
+      white: colors.white,
+      green: colors.green,
+      gray: colors.trueGray,
+      pink: colors.pink
+    },
+    extend: {
+      height: {
+        xx: '12%',
+        xs: '10%',
+        xss: '6.66%'
+      },
+      width: {
+        xx: '14%',
+        xs: '10%',
+        xss: '6.66%'
+      }
+    }
+  },
+  variants: {
+    extend: {},
+    animation: ['responsive', 'motion-safe', 'motion-reduce']
+  },
+  plugins: []
+}
